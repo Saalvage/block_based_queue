394976e3f9a93aa5687922c84b40628ff6b79112
Salvage <29021710+Saalvage@users.noreply.github.com>
2025-03-28 00:11:14 +0100

    Fix benchmark indentation and include

--- relaxed_concurrent_fifo/benchmarks/benchmark_prodcon.hpp
+++ relaxed_concurrent_fifo/benchmarks/benchmark_prodcon.hpp
@@ -6,44 +6,44 @@
 #include <iostream>
 
 struct benchmark_info_prodcon : public benchmark_info {
-	int producers;
-	int consumers;
+    int producers;
+    int consumers;
 };
 
 struct benchmark_prodcon : benchmark_default {
-	int thread_switch;
+    int thread_switch;
 
-	benchmark_prodcon(const benchmark_info& info) : benchmark_default(info) {
-		const benchmark_info_prodcon& info_prodcon = reinterpret_cast<const benchmark_info_prodcon&>(info);
-		thread_switch = info.num_threads * info_prodcon.producers / (info_prodcon.consumers + info_prodcon.producers);
-		std::cout << thread_switch << std::endl;
-	}
+    benchmark_prodcon(const benchmark_info& info) : benchmark_default(info) {
+        const benchmark_info_prodcon& info_prodcon = reinterpret_cast<const benchmark_info_prodcon&>(info);
+        thread_switch = info.num_threads * info_prodcon.producers / (info_prodcon.consumers + info_prodcon.producers);
+        std::cout << thread_switch << std::endl;
+    }
 
-	template <typename T>
-	void per_thread(int thread_index, typename T::handle& handle, std::barrier<>& a, std::atomic_bool& over) {
-		std::size_t its = 0;
-		a.arrive_and_wait();
-		while (!over) {
-			if (thread_index < thread_switch) {
-				if (handle.push(5)) {
-					its++;
-				}
-			} else {
-				if (handle.pop().has_value()) {
-					its++;
-				}
-			}
-		}
-		results[thread_index] = its;
-	}
+    template <typename T>
+    void per_thread(int thread_index, typename T::handle& handle, std::barrier<>& a, std::atomic_bool& over) {
+        std::size_t its = 0;
+        a.arrive_and_wait();
+        while (!over) {
+            if (thread_index < thread_switch) {
+                if (handle.push(5)) {
+                    its++;
+                }
+            } else {
+                if (handle.pop().has_value()) {
+                    its++;
+                }
+            }
+        }
+        results[thread_index] = its;
+    }
 
-	template <typename T>
-	void output(T& stream) {
-		stream << std::min(
-			std::reduce(results.begin(), results.begin() + thread_switch),
-			std::reduce(results.begin() + thread_switch, results.end()))
-		/ test_time_seconds;
-	}
+    template <typename T>
+    void output(T& stream) {
+        stream << std::min(
+            std::reduce(results.begin(), results.begin() + thread_switch),
+            std::reduce(results.begin() + thread_switch, results.end()))
+        / test_time_seconds;
+    }
 };
 
 #endif // BENCHMARK_PRODCON_HPP_INCLUDED
--- relaxed_concurrent_fifo/benchmarks/providers/benchmark_provider_generic.hpp
+++ relaxed_concurrent_fifo/benchmarks/providers/benchmark_provider_generic.hpp
@@ -6,20 +6,20 @@
 template <fifo FIFO, typename BENCHMARK, typename... Args>
 class benchmark_provider_generic : public benchmark_provider<BENCHMARK> {
 public:
-	benchmark_provider_generic(std::string_view name, Args... args) : name(std::vformat(name, std::make_format_args(args...))), args(args...) {}
+    benchmark_provider_generic(std::string_view name, Args... args) : name(std::vformat(name, std::make_format_args(args...))), args(args...) {}
 
-	const std::string& get_name() const override {
-		return name;
-	}
+    const std::string& get_name() const override {
+        return name;
+    }
 
-	BENCHMARK test(const benchmark_info& info, double prefill_amount) const override {
-		FIFO fifo = std::apply([&](Args... args) { return FIFO{ info.num_threads, BENCHMARK::SIZE, args... }; }, args);
-		return benchmark_provider<BENCHMARK>::template test_single<FIFO>(fifo, info, prefill_amount);
-	}
+    BENCHMARK test(const benchmark_info& info, double prefill_amount) const override {
+        FIFO fifo = std::apply([&](Args... args) { return FIFO{ info.num_threads, BENCHMARK::SIZE, args... }; }, args);
+        return benchmark_provider<BENCHMARK>::template test_single<FIFO>(fifo, info, prefill_amount);
+    }
 
 private:
-	std::string name;
-	std::tuple<Args...> args;
+    std::string name;
+    std::tuple<Args...> args;
 };
 
 #endif // BENCHMARK_PROVIDER_GENERIC_HPP_INCLUDED
