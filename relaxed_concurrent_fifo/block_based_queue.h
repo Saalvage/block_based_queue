eac54226e24d631ff1cc4c9f485a482d3eb65219
Salvage <29021710+Saalvage@users.noreply.github.com>
2025-04-01 17:13:42 +0200

    Annotate branches with `likely` and `unlikely`

--- relaxed_concurrent_fifo/block_based_queue.h
+++ relaxed_concurrent_fifo/block_based_queue.h
@@ -326,7 +326,7 @@ public:
 				}
 				index = get_read_index(ei);
 				auto write_index = get_write_index(ei);
-				if (index == write_index) {
+				if (index == write_index) [[unlikely]] {
 					// TODO: The problem here is that if epoch == (read_window + 1) we only reset the bit, which might lead to lost
 					// writes, because for the write the block header didn't change, so it fills the block, but the bit is unset.
 					// But if we simply ignore that case, then we're stuck because the bit will be set forever.
@@ -337,14 +337,14 @@ public:
 						auto diff = read_block - window.blocks;
 						window.filled_set.reset(diff, std::memory_order_relaxed);
 					}
-				} else if (index + 1 == write_index) {
+				} else if (index + 1 == write_index) [[unlikely]] {
 					if (header->epoch_and_indices.compare_exchange_weak(ei, epoch_to_header(read_epoch + 1), std::memory_order_acquire)) {
 						window_t& window = fifo.block_to_window(read_block);
 						auto diff = read_block - window.blocks;
 						window.filled_set.reset(diff, std::memory_order_relaxed);
 						break;
 					}
-				} else {
+				} else [[likely]] {
 					if (header->epoch_and_indices.compare_exchange_weak(ei, ei + (1ull << 32), std::memory_order_acquire)) {
 						break;
 					}
