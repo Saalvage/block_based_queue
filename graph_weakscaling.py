4c6ab96bf3175f1fdddc4ce5debc77ea6bf3fff6
Salvage <29021710+Saalvage@users.noreply.github.com>
2025-04-09 04:58:35 +0200

    Weak scaling script

---
+++ graph_weakscaling.py
@@ -0,0 +1,23 @@
+import sys;
+import subprocess;
+import os;
+
+exe = sys.argv[1] if len(sys.argv) >= 2 else input("Please enter the path of the executable file: ")
+graph = sys.argv[2] if len(sys.argv) >= 3 else input("Please enter the path of the executable file: ")
+graph_file = os.path.basename(graph)
+
+i = 1
+while i <= os.cpu_count():
+    subprocess.run([exe, "7", graph + str(i) + ".gr", "-t", str(i)] + sys.argv[3:], check=True)
+    i *= 2
+
+with open("weakscaling-" + graph_file + ".csv", "w") as out:
+    i = 1
+    while i <= os.cpu_count():
+        files = [f for f in os.listdir(".") if os.path.isfile(f) and graph_file + str(i) in f]
+        files.sort(reverse=True)
+        print(files[0])
+        with open(files[0]) as input:
+            for line in input:
+                out.write(line)
+        i *= 2
