3502280cc1d975fa23f1226eaedca9fc40b009b3
Salvage <29021710+Saalvage@users.noreply.github.com>
2025-04-09 06:02:34 +0200

    Weak scaling minor fixes and improvements

--- graph_weakscaling.py
+++ graph_weakscaling.py
@@ -11,13 +11,16 @@ while i <= os.cpu_count():
     subprocess.run([exe, "7", graph + str(i) + ".gr", "-t", str(i)] + sys.argv[3:], check=True)
     i *= 2
 
-with open("weakscaling-" + graph_file + ".csv", "w") as out:
+outfile = "weakscaling-" + graph_file + ".csv", "w"
+with open(outfile, "w") as out:
     i = 1
     while i <= os.cpu_count():
-        files = [f for f in os.listdir(".") if os.path.isfile(f) and graph_file + str(i) in f]
+        files = [f for f in os.listdir(".") if os.path.isfile(f) and graph_file + str(i) + ".gr" in f]
         files.sort(reverse=True)
         print(files[0])
         with open(files[0]) as input:
             for line in input:
                 out.write(line)
         i *= 2
+
+print(f"Combined results written to {outfile}")
