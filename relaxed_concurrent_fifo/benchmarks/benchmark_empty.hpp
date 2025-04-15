1138f0dadface7925e3c28354e1205b349b721eb
Salvage <29021710+Saalvage@users.noreply.github.com>
2025-04-15 11:38:44 +0200

    Less contention in fill and empty benchmarks

--- relaxed_concurrent_fifo/benchmarks/benchmark_empty.hpp
+++ relaxed_concurrent_fifo/benchmarks/benchmark_empty.hpp
@@ -7,9 +7,11 @@ struct benchmark_empty : benchmark_fill {
     template <typename T>
     void per_thread(int thread_index, typename T::handle& handle, std::barrier<>& a, std::atomic_bool& over) {
         a.arrive_and_wait();
+        std::size_t its = 0;
         while (handle.pop().has_value() && !over) {
-            results[thread_index]++;
+            its++;
         }
+        results[thread_index] = its;
     }
 };
 
