7ff7bf085e9a3bb7fa7f1e91acba078f9a67ebd2
Salvage <29021710+Saalvage@users.noreply.github.com>
2025-04-20 02:56:14 +0200

    Correctly calculate all thread counts

--- graph_weakscaling.py
+++ graph_weakscaling.py
@@ -6,21 +6,24 @@ exe = sys.argv[1] if len(sys.argv) >= 2 else input("Please enter the path of the
 graph = sys.argv[2] if len(sys.argv) >= 3 else input("Please enter the path of the executable file: ")
 graph_file = os.path.basename(graph)
 
+threads = []
 i = 1
-while i <= os.cpu_count():
-    subprocess.run([exe, "7", graph + str(i) + ".gr", "-n", "-t", str(i)] + sys.argv[3:], check=True)
+while i < os.cpu_count():
+    threads.append(i)
     i *= 2
+threads.append(os.cpu_count())
+
+for i in threads:
+    subprocess.run([exe, "7", graph + str(i) + ".gr", "-n", "-t", str(i)] + sys.argv[3:], check=True)
 
 outfile = "weakscaling-" + graph_file + ".csv"
 with open(outfile, "w") as out:
-    i = 1
-    while i <= os.cpu_count():
+    for i in threads:
         files = [f for f in os.listdir(".") if os.path.isfile(f) and graph_file + str(i) + ".gr" in f]
         files.sort(reverse=True)
         print(files[0])
         with open(files[0]) as input:
             for line in input:
                 out.write(line)
-        i *= 2
 
 print(f"Combined results written to {outfile}")
